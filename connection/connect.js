4edb2a77d0b4f4d933bf261ffb668ec721ca1528
Pragathiswaran <mpragathiswaran@gmail.com>
2024-01-23 21:11:51 +0530

    commits

--- connection/connect.js
+++ connection/connect.js
@@ -3,10 +3,16 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "praga",
-    password: "password"
+    password: "password",
+    database: "timeline"
 });
 
 con.connect(function(err){
     if(err) throw err;
     console.log("conncetion succesfull");
+    var sql = "INSERT INTO validation (username,email,phone,password) VALUES ('pragathiswaran','90909090','email@email.com','password');";
+    con.query(sql,function(err,result){
+        if(err) throw err;
+        console.log("1 record inserted");
+    })
 });
